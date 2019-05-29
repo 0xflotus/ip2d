0ca277308a0d76612a6b889fcbfc56fab1c3082f
0xflotus <0xflotus@gmail.com>
2019-05-29 16:50:45 +0200

    added more tests

--- test.ts
+++ test.ts
@@ -3,6 +3,7 @@ const { from_str, to_str } = require("./");
 test("Should be the same", () => {
   expect(from_str(to_str(1))).toBe(1);
   expect(to_str(from_str("44.2.4.0"))).toBe("44.2.4.0");
+  expect(to_str(from_str(to_str(from_str("44.2.4.0"))))).toBe("44.2.4.0");
 });
 
 test("should be 127.0.0.1", () => {
@@ -28,4 +29,5 @@ test("should also be 9.9.9.9", () => {
 test("arithmetic example", () => {
   expect(from_str("10.10.3.2") + 12).toBe(from_str("10.10.3.14"));
   expect(to_str(from_str("3.5.22.9") + 4)).toBe("3.5.22.13");
+  expect(to_str(from_str("3.5.22.255") + 2)).toBe("3.5.23.1");
 });
