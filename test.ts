45ee19790879cff2f9d5332b37a6e12dbc8fb4e1
0xflotus <0xflotus@gmail.com>
2019-05-29 16:16:32 +0200

    added more tests

--- test.ts
+++ test.ts
@@ -1,5 +1,10 @@
 const { from_str, to_str } = require("./");
 
+test("Should be the same", () => {
+  expect(from_str(to_str(1))).toBe(1);
+  expect(to_str(from_str("44.2.4.0"))).toBe("44.2.4.0");
+});
+
 test("should be 127.0.0.1", () => {
   expect(to_str(2130706433)).toBe("127.0.0.1");
 });
