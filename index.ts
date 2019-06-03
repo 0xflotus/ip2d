f91c45f91d26e46d1e78cf9577fd4a79735bfa8d
0xflotus <0xflotus@gmail.com>
2019-06-03 14:40:16 +0200

    types

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-function from_str(str: string): number {
+type IPAddress = string;
+
+function from_str(str: IPAddress): number {
   return str
     .split(".")
     .map(Number)
     .reduce((a: number, b: number) => (a << 8) | b);
 }
 
-function to_str(num: number): string {
+function to_str(num: number): IPAddress {
   return [24, 16, 8, 0].map((a: number) => (num >> a) & 0xff).join(".");
 }
 
