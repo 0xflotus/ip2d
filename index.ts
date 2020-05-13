e7df4e1ede89ba310e1829a59d286de215a9fd4d
0xflotus <0xflotus@gmail.com>
2020-05-13 16:27:45 +0200

    v1.0.11

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
 type IPAddress = string;
 
-function from_str(str: IPAddress): number {
+export function from_str(str: IPAddress): number {
   return str
     .split(".")
     .map(Number)
     .reduce((a: number, b: number) => (a << 8) | b);
 }
 
-function to_str(num: number): IPAddress {
+export function to_str(num: number): IPAddress {
   return [24, 16, 8, 0].map((a: number) => (num >> a) & 0xff).join(".");
 }
 
-export = {
-  from_str,
-  to_str
-};
