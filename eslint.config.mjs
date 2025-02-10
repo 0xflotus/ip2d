21f5e70e82c694f5844978937ed63e20205dc2b7
0xflotus <0xflotus@gmail.com>
2025-02-11 00:15:10 +0100

    chore: adjusted eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,8 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  {files: ["**/*.{ts}"]},
+  {files: ["**/*.ts"], languageOptions: {sourceType: "script"}},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
