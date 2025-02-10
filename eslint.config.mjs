78aad00cd239ea0061382ef4712e4ab21f69ab15
0xflotus <0xflotus@gmail.com>
2025-02-11 00:37:57 +0100

    chore: adjusted eslint config
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Update eslint.config.mjs
    
    * Update build-and-test.yml
    
    * Update build-and-test.yml

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,10 +2,10 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
   {
     files: ["**/*.ts"], 
-    ignores: ["dist/"],
+    ignores: ["**/*.js"],
   },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
 ];
