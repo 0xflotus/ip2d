ee51d52c40e6eb9cb608b18e0f6d7aeff0720b5b
0xflotus <0xflotus@gmail.com>
2025-02-17 20:38:52 +0100

    feat: added eslint-plugin-hex-under

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,16 @@
 import pluginJs from "@eslint/js";
+import eslintPluginHexUnder from "eslint-plugin-hex-under";
 import tseslint from "typescript-eslint";
 
 export default [
-  {
-    files: ["**/*.ts"],
-    ignores: ["**/*.js"],
-  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    plugins: {
+      "hex-under": eslintPluginHexUnder,
+    },
+    rules: {
+      "hex-under/hex-under": "error",
+    },
+  },
 ];
