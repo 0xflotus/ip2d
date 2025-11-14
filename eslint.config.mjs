98bc3906f002574f787e4f28d07af2ba50943883
0xflotus <0xflotus@gmail.com>
2025-11-14 22:31:27 +0100

    feat: add de morgan eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,12 @@
 import pluginJs from "@eslint/js";
 import tseslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import deMorgan from "eslint-plugin-de-morgan";
 import eslintPluginHexUnder from "eslint-plugin-hex-under";
 
 export default [
   pluginJs.configs.recommended,
+  deMorgan.configs.recommended,
   {
     plugins: {
       "hex-under": eslintPluginHexUnder,
