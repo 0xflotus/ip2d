cf74c6ab666968a083dc6772540f6de69ddb4a41
0xflotus <0xflotus@gmail.com>
2026-02-10 19:42:52 +0100

    feat: remove hex-under eslint plugin (#1027)
    Reason: it's incompatible with eslint@>=v10.0.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,7 @@
 import pluginJs from "@eslint/js";
 import deMorgan from "eslint-plugin-de-morgan";
-import eslintPluginHexUnder from "eslint-plugin-hex-under";
 
 export default [
   pluginJs.configs.recommended,
   deMorgan.configs.recommended,
-  {
-    plugins: {
-      "hex-under": eslintPluginHexUnder,
-    },
-    rules: {
-      "hex-under/hex-under": ["warn", { limit: 255 }],
-      "hex-under/hex-under-bigint": "warn",
-    },
-  },
 ];
