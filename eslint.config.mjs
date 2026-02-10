c732b701ffb676150d92e39542f785f53b59d36d
0xflotus <0xflotus@gmail.com>
2026-02-10 20:10:42 +0100

    feat: add hex-under eslint plugin (#1028)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,17 @@
 import pluginJs from "@eslint/js";
 import deMorgan from "eslint-plugin-de-morgan";
+import hexUnder from "eslint-plugin-hex-under";
 
 export default [
   pluginJs.configs.recommended,
   deMorgan.configs.recommended,
+  {
+    plugins: {
+      "hex-under": hexUnder,
+    },
+    rules: {
+      "hex-under/hex-under": ["warn", { limit: 255 }],
+      "hex-under/hex-under-bigint": "warn",
+    },
+  },
 ];
